8488e761a7aeff4ae8c91c865e12a3eedfd7722f
Maksym Babalich <17033001m@gmail.com>
2026-09-20 14:29:01 +0300

    sol

---
+++ src/components/PressedKeyMessage.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+type State = {
+  key: string | null;
+};
+
+export class PressedKeyMessage extends React.Component {
+  state: State = {
+    key: null,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ key: event.key });
+  };
+
+  render(): React.ReactNode {
+    const { key } = this.state;
+
+    return (
+      <div className="App">
+        {key ? (
+          <p className="App__message">The last pressed key is [{key}]</p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
+      </div>
+    );
+  }
+}
